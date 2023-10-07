60dbc3b67e3b58e5aabd0296825b7ec8a10bc5a5
Alejandro Malvacias <alejandromalvacias1@gmail.com>
2023-10-07 17:11:01 -0400

    List of cryptos created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-
-
+import { useEffect } from "react"
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
 
-  return (
-    <h1>Hola Mundo</h1>
+
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    const [cryptos, setCryptos] = useState()
+
+    useEffect(() => {
+        axios.get(`${ API_URL }assets`)
+            .then((data) => {
+                console.log(data)
+                setCryptos(data.data.data);
+            })
+            .catch(() => {
+                console.error("Error fetching")
+            });
+    }, [])
+
+    if (!cryptos) return <span>Cargando...</span>
+
+
+    return (
+    <>
+        <h1>Lista de Cryptos</h1>
+        
+        <ol>
+            {
+                cryptos.map(({ id, name, priceUsd }) => (
+                    <li key={id}>Nombre: {name} Precio: {priceUsd }</li>
+                ))
+            }        
+        </ol>
+    
+    
+    </>
+      
+    
+      
+      
   )
 }
 
